680071a3dee61ce25c9d0c6e8f0bafea266d9edd
Dani Coleman <danicoleman00@gmail.com>
2020-09-19 11:40:24 -0600

    Add bulk discount relationship to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :order_items, through: :items
   has_many :orders, through: :order_items
-  has_many :users
+  has_many :users, dependent: :destroy
+  has_many :bulk_discounts, dependent: :destroy
 
   validates_presence_of :name,
                         :address,
