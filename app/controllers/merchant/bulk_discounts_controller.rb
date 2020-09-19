b90b0e5fe845dec9deee73616dadd29362f4276e
Dani Coleman <danicoleman00@gmail.com>
2020-09-19 12:46:24 -0600

    Add new action to bulk discounts controller

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -3,4 +3,8 @@ class Merchant::BulkDiscountsController < Merchant::BaseController
   def index
     @discounts = current_user.merchant.bulk_discounts
   end
+
+  def new
+    @discount = BulkDiscount.new
+  end
 end
