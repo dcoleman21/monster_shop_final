ac7be45d332474e05ee70d220ef477fa5d7c3e8c
Dani Coleman <danicoleman00@gmail.com>
2020-09-21 17:09:20 -0600

    Change merchant associated with the discounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ Merchant.destroy_all
 User.destroy_all
 megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-merchant_user = megan.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan_1@example.com', password: 'securepassword')
+merchant_user = brian.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan_1@example.com', password: 'securepassword')
+discount1 = BulkDiscount.create!(discount_percentage: 5, item_minimun: 6, merchant_id: brian.id)
+discount2 = BulkDiscount.create!(discount_percentage: 10, item_minimun: 12, merchant_id: brian.id)
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-discount1 = BulkDiscount.create!(discount_percentage: 5, item_minimun: 6, merchant_id: megan.id)
-discount2 = BulkDiscount.create!(discount_percentage: 10, item_minimun: 12, merchant_id: megan.id)
