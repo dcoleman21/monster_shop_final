f188078b92dadb1a8f044a2842f8b8852f99314a
Dani Coleman <danicoleman00@gmail.com>
2020-09-21 16:00:07 -0600

    Add a create action in the bulk discounts controller

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -7,4 +7,14 @@ class Merchant::BulkDiscountsController < Merchant::BaseController
   def new
     @discount = BulkDiscount.new
   end
+
+  def create
+    current_user.merchant.bulk_discounts.create(bulk_discount_params)
+    redirect_to '/merchant/bulk_discounts'
+  end
+
+  private
+  def bulk_discount_params
+    params.permit(:discount_percentage, :item_minimun)
+  end
 end
