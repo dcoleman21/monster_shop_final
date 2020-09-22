13b47d488f96aaaa146643b29bf4f8962972c3f1
Dani Coleman <danicoleman00@gmail.com>
2020-09-22 15:02:54 -0600

    Add a max_discount method in the merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -33,4 +33,8 @@ class Merchant < ApplicationRecord
   def order_items_by_order(order_id)
     order_items.where(order_id: order_id)
   end
+
+  def max_discount(item_quantity)
+    bulk_discounts.where("#{item_quantity} >= item_minimun").maximum(:discount_percentage)
+  end
 end
