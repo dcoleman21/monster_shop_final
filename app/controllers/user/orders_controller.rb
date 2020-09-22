101c4411e6f8cf10fc3a176091fa04808c6018dc
Dani Coleman <danicoleman00@gmail.com>
2020-09-22 14:23:47 -0600

    Add helper methods to orders controller

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -12,13 +12,7 @@ class User::OrdersController < ApplicationController
   def create
     order = current_user.orders.new
     order.save
-      cart.items.each do |item|
-        order.order_items.create({
-          item: item,
-          quantity: cart.count_of(item.id),
-          price: item.price
-          })
-      end
+    create_order_items(order)
     session.delete(:cart)
     flash[:notice] = "Order created successfully!"
     redirect_to '/profile/orders'
@@ -28,5 +22,24 @@ class User::OrdersController < ApplicationController
     order = current_user.orders.find(params[:id])
     order.cancel
     redirect_to "/profile/orders/#{order.id}"
+  end
+
+  private
+
+  def create_single_order_item(order, item)
+    order.order_items.create({item: item, quantity: cart.count_of(item.id), price: item.price})
+  end
+
+  def discount_order_item(item, order_item)
+    if cart.item_minimun_met?(item.id)
+      order_item.apply_discount(cart.applied_discount(item.id))
+    end
+  end
+
+  def create_order_items(order)
+    cart.items.each do |item|
+      order_item = create_single_order_item(order, item)
+      discount_order_item(item, order_item)
+    end
   end
 end
